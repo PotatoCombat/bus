8c2dec7fcee362ec1e4df1cb580594cd3c1a7e32
Pyrolf <gregorykoh96@gmail.com>
2024-12-10 17:02:43 +0800

    FE: Add MapView

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,15 +1,29 @@
-import { Text, View } from "react-native";
+import { View, StyleSheet } from "react-native";
+
+import MapView from "react-native-maps";
+const styles = StyleSheet.create({
+ container: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },  
+});
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        region={{
+          latitude: 1.364917,
+          longitude: 103.822872,
+          latitudeDelta: 0,
+          longitudeDelta: 0.55,
+        }}
+      />
     </View>
   );
 }
