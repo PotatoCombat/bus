7fafbabcfac49a4b55ca40a1b0314b08fe4eb091
Pyrolf <gregorykoh96@gmail.com>
2024-12-12 16:56:07 +0800

    Display search results from mock data

--- frontend/app/components/search/styles.tsx
+++ frontend/app/components/search/styles.tsx
@@ -1,5 +1,23 @@
 import { StyleSheet } from 'react-native';
 
+const commonStyles = StyleSheet.create({
+    inputContainer: {
+        backgroundColor: "white",
+    },
+    result: {
+        backgroundColor: 'white',
+        maxHeight: 56,
+        minHeight: 56,
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        flex: 1,
+        padding: 'auto',
+        paddingLeft: 20,
+        paddingRight: 20,
+    },
+});
+
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "transparent",
@@ -8,13 +26,61 @@ const styles = StyleSheet.create({
     padding: 0,
   },
   inputContainer: {
-    backgroundColor: "white",
+    ...commonStyles.inputContainer,
     borderRadius: 28,
+  },
+  searchedInputContainer: {
+    ...commonStyles.inputContainer,
+    borderTopStartRadius: 28,
+    borderTopEndRadius: 28,
+    borderBottomColor: "gray",
+    borderBottomWidth: 2,
   },
   input: {
     color: "black",
-    padding: 16,
+    fontSize: 20,
+    marginLeft: 20,
+    maxHeight: 56,
+    minHeight: 56,
   },
+  rightIconContainer: {
+    marginRight: 17,
+    marginVertical: 0,
+    paddingRight: 0,
+  },
+  result: {
+    ...commonStyles.result,
+  },
+  lastResult: {
+    ...commonStyles.result,
+    borderBottomLeftRadius: 28,
+    borderBottomRightRadius: 28,
+  },
+  noResult: {
+    textAlign: 'center',
+    width: '100%',
+    fontSize: 14,
+  },
+  serviceNoContainer: {
+    backgroundColor: "lightgray",
+    borderRadius: 6,
+    paddingHorizontal: 13,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  serviceNo: {
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  roadNameContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 10,
+    maxWidth: '35%',
+  },
+  roadName: {
+    fontSize: 14,
+  }
 });
 
 export default styles;
