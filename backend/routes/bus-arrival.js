83fa8910bbf76244b42b0d45006929c1b665abc5
ZhaoYiiii <zhaoyiwuu@gmail.com>
2024-12-13 17:01:43 +0800

    return -1 when null

--- backend/routes/bus-arrival.js
+++ backend/routes/bus-arrival.js
@@ -3,16 +3,15 @@ const router = require('express').Router();
 const { fetchFromLTA } = require('../utils/fetchFromLTA');
 
 // Define a function called diff_minutes that calculates the difference in minutes between two Date objects (dt2 and dt1)
-function diff_minutes(dt1) 
+function diff_minutes(dt) 
  {
-    if(dt1 == ''){
+    if(dt == '' || dt == null){
         return -1;
     }else{
-        let currentTime = new Date();
-        var x = new Date(dt1);
-        var y = new Date(currentTime);
+        var x = new Date(dt);
+        var currentTime = new Date();
         // Calculate the difference in milliseconds between the two provided dates and convert it to seconds
-        var diff =(x.getTime() - y.getTime()) / 1000;
+        var diff =(x.getTime() - currentTime.getTime()) / 1000;
         // Convert the difference from seconds to minutes
         diff /= 60;
         // Return the absolute value of the rounded difference in minutes
@@ -36,9 +35,9 @@ router.get('/', async (req, res) => {
             timings.push({
                 ServiceNo : bus.ServiceNo,
                 NextBuses : [
-                    diff_minutes(new Date(bus.NextBus.EstimatedArrival)),
-                    diff_minutes(new Date(bus.NextBus2.EstimatedArrival)),
-                    diff_minutes(new Date(bus.NextBus3.EstimatedArrival))
+                    diff_minutes(bus.NextBus.EstimatedArrival),
+                    diff_minutes(bus.NextBus2.EstimatedArrival),
+                    diff_minutes(bus.NextBus3.EstimatedArrival)
                 ]
             });
         });
