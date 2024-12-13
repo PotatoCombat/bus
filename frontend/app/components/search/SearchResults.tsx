e4b1c3db4e608357c1816d95800dfe68fbbec233
Pyrolf <gregorykoh96@gmail.com>
2024-12-13 10:32:26 +0800

    FIx search results bug (unable to interact  map)

--- frontend/app/components/search/SearchResults.tsx
+++ frontend/app/components/search/SearchResults.tsx
@@ -1,5 +1,5 @@
 import { FlatList, Text, View } from "react-native";
-import styles from "./styles";
+import styles, { ROW_HEIGHT } from "./styles";
 import SearchResultInterface from "@/app/types/SearchResultInterface";
 import { Icon } from "react-native-elements";
 import { noBusesFound } from "@/app/utils/strings";
@@ -18,35 +18,37 @@ export default function SearchResults({
               <Text style={styles.noResult}>{noBusesFound}</Text>
             </View>
           ) : (
-            <FlatList
-              data={results}
-              renderItem={({ item, index }) => (
-                <View
-                  key={item.serviceNo + index}
-                  style={
-                    index < results.length - 1
-                      ? styles.result
-                      : styles.lastResult
-                  }
-                >
-                  <View style={styles.serviceNoContainer}>
-                    <Text style={styles.serviceNo}>{item.serviceNo}</Text>
-                  </View>
+            <View style={{ height: results.length * ROW_HEIGHT }}>
+              <FlatList
+                data={results}
+                renderItem={({ item, index }) => (
+                  <View
+                    key={item.serviceNo + index}
+                    style={
+                      index < results.length - 1
+                        ? styles.result
+                        : styles.lastResult
+                    }
+                  >
+                    <View style={styles.serviceNoContainer}>
+                      <Text style={styles.serviceNo}>{item.serviceNo}</Text>
+                    </View>
 
-                  <View style={styles.roadNameContainer}>
-                    <Text style={styles.roadName}>{item.originRoadName}</Text>
-                  </View>
+                    <View style={styles.roadNameContainer}>
+                      <Text style={styles.roadName}>{item.originRoadName}</Text>
+                    </View>
 
-                  <Icon name="arrow-right" />
+                    <Icon name="arrow-right" />
 
-                  <View style={styles.roadNameContainer}>
-                    <Text style={styles.roadName}>
-                      {item.destinationRoadName}
-                    </Text>
+                    <View style={styles.roadNameContainer}>
+                      <Text style={styles.roadName}>
+                        {item.destinationRoadName}
+                      </Text>
+                    </View>
                   </View>
-                </View>
-              )}
-            />
+                )}
+              />
+            </View>
           )}
         </>
       )}
