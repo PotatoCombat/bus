9ec7667e2926f60962782ba9b518d28bd0d1e495
ZhaoYiiii <zhaoyiwuu@gmail.com>
2024-12-13 16:55:41 +0800

    refacter "check if null" statements into method "diff_minutes"

--- backend/routes/bus-arrival.js
+++ backend/routes/bus-arrival.js
@@ -3,36 +3,42 @@ const router = require('express').Router();
 const { fetchFromLTA } = require('../utils/fetchFromLTA');
 
 // Define a function called diff_minutes that calculates the difference in minutes between two Date objects (dt2 and dt1)
-function diff_minutes(dt1, dt2) 
+function diff_minutes(dt1) 
  {
-    var x = new Date(dt1);
-    var y = new Date(dt2);
-    // Calculate the difference in milliseconds between the two provided dates and convert it to seconds
-    var diff =(x.getTime() - y.getTime()) / 1000;
-    // Convert the difference from seconds to minutes
-    diff /= 60;
-    // Return the absolute value of the rounded difference in minutes
-    return Math.abs(Math.round(diff));
+    if(dt1 == ''){
+        return -1;
+    }else{
+        let currentTime = new Date();
+        var x = new Date(dt1);
+        var y = new Date(currentTime);
+        // Calculate the difference in milliseconds between the two provided dates and convert it to seconds
+        var diff =(x.getTime() - y.getTime()) / 1000;
+        // Convert the difference from seconds to minutes
+        diff /= 60;
+        // Return the absolute value of the rounded difference in minutes
+        var mins = Math.abs(Math.round(diff));
+
+        return mins;
+    }
+    
  }
 
 router.get('/', async (req, res) => {
     var busStopCode = req.query.BusStopCode;
     await fetchFromLTA('https://datamall2.mytransport.sg/ltaodataservice/v3/BusArrival?BusStopCode=' + busStopCode)
     .then(response => {
-        lastTimestamp = new Date(response.headers.get('Date'));
         return response.json();
     })
     .then(json => {
-        let lastTimestamp = new Date();
         buses = json.Services;
         timings = [];
         buses.forEach(bus => {
             timings.push({
                 ServiceNo : bus.ServiceNo,
                 NextBuses : [
-                    (bus.NextBus.EstimatedArrival != '')?diff_minutes(new Date(bus.NextBus.EstimatedArrival), lastTimestamp) : -1,
-                    (bus.NextBus2.EstimatedArrival != '')?diff_minutes(new Date(bus.NextBus2.EstimatedArrival), lastTimestamp) : -1,
-                    (bus.NextBus3.EstimatedArrival != '')?diff_minutes(new Date(bus.NextBus3.EstimatedArrival), lastTimestamp) : -1
+                    diff_minutes(new Date(bus.NextBus.EstimatedArrival)),
+                    diff_minutes(new Date(bus.NextBus2.EstimatedArrival)),
+                    diff_minutes(new Date(bus.NextBus3.EstimatedArrival))
                 ]
             });
         });
