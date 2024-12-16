c0a5e96b1e8be60587f33c51128214d749fb3e27
ZhaoYiiii <66521076+ZhaoYiiii@users.noreply.github.com>
2024-12-16 15:05:48 +0800

    Backend: Add arrival timings API (#5)
    * modify NextBuses into an array instead
    
    * return -1 when estimated time is null
    
    * refacter "check if null" statements into method "diff_minutes"
    
    * return -1 when null

--- backend/routes/bus-arrival.js
+++ backend/routes/bus-arrival.js
@@ -2,35 +2,30 @@ const router = require('express').Router();
 
 const { fetchFromLTA } = require('../utils/fetchFromLTA');
 
-let lastResult = undefined;
-let lastTimestamp = new Date();
-
-let timeToLive = 10000;
-
-let data = new Map();
-
 // Define a function called diff_minutes that calculates the difference in minutes between two Date objects (dt2 and dt1)
-function diff_minutes(dt1, dt2) 
+function diff_minutes(dt) 
  {
-    var x = new Date(dt1);
-    var y = new Date(dt2);
-    // Calculate the difference in milliseconds between the two provided dates and convert it to seconds
-    var diff =(x.getTime() - y.getTime()) / 1000;
-    // Convert the difference from seconds to minutes
-    diff /= 60;
-    // Return the absolute value of the rounded difference in minutes
-    return Math.abs(Math.round(diff));
+    if(dt == '' || dt == null){
+        return -1;
+    }else{
+        var x = new Date(dt);
+        var currentTime = new Date();
+        // Calculate the difference in milliseconds between the two provided dates and convert it to seconds
+        var diff =(x.getTime() - currentTime.getTime()) / 1000;
+        // Convert the difference from seconds to minutes
+        diff /= 60;
+        // Return the absolute value of the rounded difference in minutes
+        var mins = Math.abs(Math.round(diff));
+
+        return mins;
+    }
+    
  }
 
 router.get('/', async (req, res) => {
-    if (lastResult && (new Date() - lastTimestamp) < timeToLive) {
-        res.send(lastResult);
-        return;
-    }
     var busStopCode = req.query.BusStopCode;
     await fetchFromLTA('https://datamall2.mytransport.sg/ltaodataservice/v3/BusArrival?BusStopCode=' + busStopCode)
     .then(response => {
-        lastTimestamp = new Date(response.headers.get('Date'));
         return response.json();
     })
     .then(json => {
@@ -39,12 +34,13 @@ router.get('/', async (req, res) => {
         buses.forEach(bus => {
             timings.push({
                 ServiceNo : bus.ServiceNo,
-                NextBus1 : diff_minutes(new Date(bus.NextBus.EstimatedArrival), lastTimestamp),
-                NextBus2 : diff_minutes(new Date(bus.NextBus2.EstimatedArrival), lastTimestamp),
-                NextBus3 : diff_minutes(new Date(bus.NextBus3.EstimatedArrival), lastTimestamp)
+                NextBuses : [
+                    diff_minutes(bus.NextBus.EstimatedArrival),
+                    diff_minutes(bus.NextBus2.EstimatedArrival),
+                    diff_minutes(bus.NextBus3.EstimatedArrival)
+                ]
             });
         });
-        lastResult = timings;
         res.send({
             BusStopCode : busStopCode,
             Timings : timings
