373eb54b1ae13a1465b0b64914c5db912357d659
Pyrolf <gregorykoh96@gmail.com>
2024-12-16 14:38:14 +0800

    Add search result display and search button upon clicking a search result

--- frontend/app/components/search/Search.tsx
+++ frontend/app/components/search/Search.tsx
@@ -1,48 +1,80 @@
 import { useState } from "react";
-import { StyleProp, View, ViewStyle } from "react-native";
-import { SearchBar } from 'react-native-elements';
+import { StyleProp, TouchableHighlight, View, ViewStyle } from "react-native";
+import { Icon, SearchBar } from "react-native-elements";
 import styles from "./styles";
 import SearchResultInterface from "@/app/types/SearchResultInterface";
 import searchApi from "@/app/services/SearchApi";
 import SearchResults from "./SearchResults";
 import { searchBarPlaceholderText } from "@/app/utils/strings";
+import SearchResultDisplay from "./SearchResultDisplay";
 
 export default function Search({ style }: { style: StyleProp<ViewStyle> }) {
   const [value, setValue] = useState("");
+  const [selectedResult, setSelectedResult] = useState<SearchResultInterface | null>(null);
   const [results, setResults] = useState<SearchResultInterface[] | null>(null);
 
   const changeText = (text: string) => {
     setValue(text);
+
     if (text.length == 0) {
       setResults(null);
     } else {
       setResults(searchApi(text));
     }
-  };  
+  };
+
+  const handleClickSearchButton = () => {
+    setSelectedResult(null);
+  };
 
   return (
     <View style={style}>
-      <SearchBar
-        containerStyle={styles.container}
-        inputContainerStyle={
-          results != null
-            ? styles.searchedInputContainer
-            : styles.inputContainer
-        }
-        inputStyle={styles.input}
-        placeholder={searchBarPlaceholderText}
-        onChangeText={changeText}
-        value={value}
-        clearIcon={{
-          color: "black",
-          size: 24,
-        }}
-        rightIconContainerStyle={styles.rightIconContainer}
-        searchIcon={() => {}}
-        lightTheme={true}
-      />
+      {selectedResult == null ? (
+        <>
+          <SearchBar
+            containerStyle={styles.searchBarContainer}
+            inputContainerStyle={
+              results != null
+                ? styles.searchedInputContainer
+                : styles.inputContainer
+            }
+            inputStyle={styles.input}
+            placeholder={searchBarPlaceholderText}
+            onChangeText={changeText}
+            value={value}
+            clearIcon={{
+              color: "black",
+              size: 24,
+            }}
+            rightIconContainerStyle={styles.rightIconContainer}
+            searchIcon={() => {}}
+            lightTheme={true}
+          />
 
-      <SearchResults results={results} />
+          <SearchResults
+            results={results}
+            setSelectedResult={setSelectedResult}
+          />
+        </>
+      ) : (
+        <View style={styles.rowContainer}>
+          <SearchResultDisplay
+            selectedResult={selectedResult}
+            setSelectedResult={setSelectedResult}
+          />
+
+          <TouchableHighlight
+            onPress={handleClickSearchButton}
+            style={styles.buttonIconContainer}
+            activeOpacity={0.6}
+            underlayColor="#EEEEEE"
+          >
+            <View>
+              <Icon name="search" />
+            </View>
+          </TouchableHighlight>
+        </View>
+      )}
     </View>
   );
 }
