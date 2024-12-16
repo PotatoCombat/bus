3caf3565cc3e96a04b4870c389ccca3d8787c6ea
Pyrolf <gregorykoh96@gmail.com>
2024-12-16 14:53:52 +0800

    Restyle search results

--- frontend/app/services/SearchApi.tsx
+++ frontend/app/services/SearchApi.tsx
@@ -1,6 +1,6 @@
 import SearchResultInterface from '../types/SearchResultInterface'
 import { mockSearchResults } from '../utils/mockData';
 
-export default function searchApi(serviceNo: string): SearchResultInterface[] {
+export default function searchApi(serviceNo: string): SearchResultInterface[] {  
   return serviceNo == "88" ? mockSearchResults : [];
 }
