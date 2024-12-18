ae95e2e0049211788074e3c955d1535dd25e17d6
tengsokee <tengsokee@protonmail.com>
2024-12-18 16:16:39 +0800

    Clean up code and BottomSheet matches Figma

--- frontend/app/components/BottomSheet/ListRow.tsx
+++ frontend/app/components/BottomSheet/ListRow.tsx
@@ -1,70 +1,64 @@
 import { mockBusArrival } from "@/app/utils/mockData";
-import { View, StyleSheet, StatusBar, Text, Button } from "react-native";
+import { View, StyleSheet, StatusBar, Text } from "react-native";
 import { FlatList } from "react-native-gesture-handler";
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        marginTop: StatusBar.currentHeight || 0,
-    },
-    item: {
-        backgroundColor: '#f9c2ff',
-        height: 60,
-        marginVertical: 8,
-        flex: 1,
-        justifyContent: "center",
-    },
-    arrival:{
-        backgroundColor: 'green',
-        flex:1,
-    },
-    blank:{
-        flex:1,
-
-    },
-    title: {
-        fontSize: 32,
-        textAlign:'center',
-    },
+  item: {
+    paddingLeft: 12,
+    height: 60,
+    marginVertical: 8,
+    flex: 1,
+    justifyContent: "center",
+  },
+  arrival: {
+    borderRadius: 8,
+    backgroundColor: "#D9D9D9",
+    margin: 8,
+    justifyContent: "center",
+    flex: 1,
+  },
+  arrivalText: {
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  blank: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 32,
+    textAlign: "center",
+  },
 });
 
-const Item = ({ serviceNo }) => (
-    <View style={styles.item}>
-        <Text style={styles.title}>{serviceNo}</Text>
-    </View>
+const Item = ({ serviceNo }: { serviceNo: any }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{serviceNo}</Text>
+  </View>
 );
 
-
-
 export default function ListRow() {
-
-    return (
-
-        <View style={{ flex: 1, flexDirection: "row" }}>
-            <FlatList
-
-                data={mockBusArrival}
-                renderItem={({ item }) =>
-                    <View style={{ flex: 1, flexDirection: "row" }}>
-                        <Item serviceNo={item.serviceNo} />
-                        <Text style={styles.blank}>blank</Text>
-                        <View style={{flex:1,}}><Button onPress={() => null} title="12 mins"/></View>
-                        <Text style={styles.arrival}>button</Text>
-                        <View style={{
-                            backgroundColor: '#f9c2ff',
-                            padding: 20,
-                            marginVertical: 8,
-                            marginHorizontal: 16,
-                            flex: 1,
-                        }}><Text>hellow</Text></View>
-
-                    </View>
-
-
-                }
-                keyExtractor={item => item.busStopNo}
-            />
-
-        </View>
-    );
+  return (
+    <View style={{ flex: 1, flexDirection: "row" }}>
+      <FlatList
+        data={mockBusArrival}
+        renderItem={({ item }) => (
+          <View style={{ flex: 1, flexDirection: "row" }}>
+            <Item serviceNo={item.bus1} />
+            <Text style={styles.blank}></Text>
+            <View style={styles.arrival}>
+              <Text style={styles.arrivalText}>{item.bus1}</Text>
+            </View>
+            <View style={styles.arrival}>
+              <Text style={styles.arrivalText}>{item.bus2}</Text>
+            </View>
+            <View style={styles.arrival}>
+              <Text style={styles.arrivalText}>{item.bus3}</Text>
+            </View>
+          </View>
+        )}
+        keyExtractor={(item) => item.busStopNo}
+      />
+    </View>
+  );
 }
