f524faec448884bf2c37304b70296d6b283fa659
ZhaoYiiii <zhaoyiwuu@gmail.com>
2024-12-20 13:17:15 +0800

    use .map() function

--- backend/routes/bus-route.js
+++ backend/routes/bus-route.js
@@ -5,11 +5,7 @@ router.get('/', async (req, res) => {
     if (req.query.Direction) {
         let busService = req.busServices.get(req.query.ServiceNo.toString()).get(req.query.Direction.toString());
         let stopsOfDirection = busRoutes.get(req.query.Direction);
-        let busStops = [];
-        stopsOfDirection.forEach(stop =>{
-            busStops.push(stop.BusStopCode);
-        });
-        
+        let busStops = stopsOfDirection.map((x) =>x.BusStopCode);
         res.send({
             ServiceNo : req.query.ServiceNo,
             Routes : [{
