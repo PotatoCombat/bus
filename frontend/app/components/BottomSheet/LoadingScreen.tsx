2d55073c904820306115f3b7f3d429a247aea7ad
tengsokee <tengsokee@protonmail.com>
2024-12-20 16:15:46 +0800

    if bus timing is -1, diagonal line is shown

--- frontend/app/components/BottomSheet/LoadingScreen.tsx
+++ frontend/app/components/BottomSheet/LoadingScreen.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { View, Text, ActivityIndicator, StyleSheet } from 'react-native';
+import { View, Text, ActivityIndicator, StyleSheet } from "react-native";
 
 const LoadingScreen = () => {
   return (
@@ -13,24 +12,24 @@ const LoadingScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    width: '100%',
-    height:'100%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',  // Semi-transparent background
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)", // Semi-transparent background
   },
-    loadingContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '100%',
-    height:'100%',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  loadingContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    height: "100%",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
     flex: 1,
   },
   loadingText: {
     marginTop: 10,
     fontSize: 16,
-    color: 'white',
+    color: "white",
   },
 });
 
