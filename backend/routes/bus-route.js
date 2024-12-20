c77c14988952691cf4326c0a3a907ac4e57da583
ZhaoYiiii <zhaoyiwuu@gmail.com>
2024-12-20 11:00:29 +0800

    implement use of busServices cache

--- backend/routes/bus-route.js
+++ backend/routes/bus-route.js
@@ -3,13 +3,13 @@ const router = require('express').Router();
 router.get('/', async (req, res) => {
     let busRoutes = req.busRoutes.get(req.query.ServiceNo);
     if (req.query.Direction) {
-        let busService = req.busServices.get(req.query.ServiceNo, req.query.Direction);
+        let busService = req.busServices.get(req.query.ServiceNo.toString()).get(req.query.Direction.toString());
         let stopsOfDirection = busRoutes.get(req.query.Direction);
         let busStops = [];
         stopsOfDirection.forEach(stop =>{
             busStops.push(stop.BusStopCode);
         });
-
+        
         res.send({
             ServiceNo : req.query.ServiceNo,
             Routes : [{
@@ -23,7 +23,7 @@ router.get('/', async (req, res) => {
         let direction = 1;
         busRoutes.forEach(route => {
             let busStops = [];
-            let busService = req.busServices.get(req.query.ServiceNo, req.query.Direction);
+            let busService = req.busServices.get(req.query.ServiceNo.toString()).get(direction.toString());
             route.forEach(stop => {
                 busStops.push(stop.BusStopCode);
             });
