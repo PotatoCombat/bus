47b5c9a629fd026677bf1a6f41a680047c92f875
ZhaoYiiii <zhaoyiwuu@gmail.com>
2024-12-20 17:14:35 +0800

    remove console.log

--- backend/routes/bus-stop.js
+++ backend/routes/bus-stop.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => {
     let busStopCode = req.query.BusStopCode;
-    console.log(busStopCode);
     res.send(req.busStops.get(busStopCode.toString()));
 });
 
