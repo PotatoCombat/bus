dbeba9b9efb9cbf9967acb3a6a9e97b7efe81de8
Gabriel Teo <gtforce212@gmail.com>
2024-12-23 03:50:18 +0800

    FE: Add bus route
    FE: Connect search bar and bus route
    
    FE: Add colours to bus stop and routes
    
    FE: Add last bus stop marker
    
    FE: Refactor bus stop selection
    
    FE: Add TODOS

---
+++ frontend/app/utils/mockBusRoute.tsx
@@ -0,0 +1,219 @@
+export const mockBusRoute = [
+    {
+        "BusStopCode": "67009",
+        "RoadName": "Sengkang Sq",
+        "Description": "Sengkang Int",
+        "Latitude": 1.39178060381601,
+        "Longitude": 103.89588499436073
+    },
+    {
+        "BusStopCode": "67191",
+        "RoadName": "Compassvale Rd",
+        "Description": "Blk 223D",
+        "Latitude": 1.38908889970693,
+        "Longitude": 103.89710881285436
+    },
+    {
+        "BusStopCode": "67171",
+        "RoadName": "Compassvale Dr",
+        "Description": "Blk 225A",
+        "Latitude": 1.38731209583307,
+        "Longitude": 103.89897692182004
+    },
+    {
+        "BusStopCode": "67139",
+        "RoadName": "Rivervale Dr",
+        "Description": "Nth Spring Pr Sch",
+        "Latitude": 1.38715714684677,
+        "Longitude": 103.90417577185805
+    },
+    {
+        "BusStopCode": "67229",
+        "RoadName": "Rivervale Dr",
+        "Description": "Rumbia Stn Exit A",
+        "Latitude": 1.39105777803334,
+        "Longitude": 103.90614361099206
+    },
+    {
+        "BusStopCode": "67239",
+        "RoadName": "Rivervale Dr",
+        "Description": "Opp Rivervale Pr Sch",
+        "Latitude": 1.3930503019817,
+        "Longitude": 103.90373341553943
+    },
+    {
+        "BusStopCode": "65061",
+        "RoadName": "Punggol Rd",
+        "Description": "Blk 298A",
+        "Latitude": 1.39490129020977,
+        "Longitude": 103.9018594759131
+    },
+    {
+        "BusStopCode": "68119",
+        "RoadName": "Jln Kayu",
+        "Description": "Aft Seletar Camp G",
+        "Latitude": 1.402186,
+        "Longitude": 103.872168
+    },
+    {
+        "BusStopCode": "68059",
+        "RoadName": "Jln Kayu",
+        "Description": "Aft TPE",
+        "Latitude": 1.39978638898633,
+        "Longitude": 103.87243527803194
+    },
+    {
+        "BusStopCode": "68049",
+        "RoadName": "Jln Kayu",
+        "Description": "Opp Jln Tari Lilin",
+        "Latitude": 1.39851660309414,
+        "Longitude": 103.8729604565282
+    },
+    {
+        "BusStopCode": "68039",
+        "RoadName": "Jln Kayu",
+        "Description": "Aft Abundant Grace Ch",
+        "Latitude": 1.39652972197871,
+        "Longitude": 103.87311388899468
+    },
+    {
+        "BusStopCode": "68029",
+        "RoadName": "Jln Kayu",
+        "Description": "Blk 448A",
+        "Latitude": 1.39312162118162,
+        "Longitude": 103.87264955115819
+    },
+    {
+        "BusStopCode": "68019",
+        "RoadName": "Sengkang West Ave",
+        "Description": "Aft Jln Kayu",
+        "Latitude": 1.39106694073297,
+        "Longitude": 103.87060612962611
+    },
+    {
+        "BusStopCode": "67089",
+        "RoadName": "Sengkang West Rd",
+        "Description": "Bef Fernvale Lane",
+        "Latitude": 1.39029406445756,
+        "Longitude": 103.86916019529639
+    },
+    {
+        "BusStopCode": "67039",
+        "RoadName": "Yio Chu Kang Rd",
+        "Description": "Aft Seletar Rd",
+        "Latitude": 1.38831000001759,
+        "Longitude": 103.86800750000182
+    },
+    {
+        "BusStopCode": "67029",
+        "RoadName": "Yio Chu Kang Rd",
+        "Description": "Aft Dedap Rd",
+        "Latitude": 1.38906690681702,
+        "Longitude": 103.86517736947857
+    },
+    {
+        "BusStopCode": "67019",
+        "RoadName": "Yio Chu Kang Rd",
+        "Description": "Aft Begonia Rd",
+        "Latitude": 1.38976371710347,
+        "Longitude": 103.86253811109862
+    },
+    {
+        "BusStopCode": "55109",
+        "RoadName": "Yio Chu Kang Rd",
+        "Description": "Serenity Pk",
+        "Latitude": 1.39049447273752,
+        "Longitude": 103.8599334800352
+    },
+    {
+        "BusStopCode": "55089",
+        "RoadName": "Yio Chu Kang Rd",
+        "Description": "Aft Sunrise Gdns",
+        "Latitude": 1.39129403706334,
+        "Longitude": 103.85630398734284
+    },
+    {
+        "BusStopCode": "55079",
+        "RoadName": "Yio Chu Kang Rd",
+        "Description": "Aft Econ Medicare Ctr",
+        "Latitude": 1.391952,
+        "Longitude": 103.853366
+    },
+    {
+        "BusStopCode": "55059",
+        "RoadName": "Yio Chu Kang Rd",
+        "Description": "ST Electronics",
+        "Latitude": 1.3920822654425,
+        "Longitude": 103.84909518015897
+    },
+    {
+        "BusStopCode": "55049",
+        "RoadName": "Yio Chu Kang Rd",
+        "Description": "Bef AMK St 64/Apple SG",
+        "Latitude": 1.39110547694961,
+        "Longitude": 103.84659281119058
+    },
+    {
+        "BusStopCode": "55039",
+        "RoadName": "Yio Chu Kang Rd",
+        "Description": "NCS Hub",
+        "Latitude": 1.3891573228776,
+        "Longitude": 103.84383135324573
+    },
+    {
+        "BusStopCode": "55179",
+        "RoadName": "Ang Mo Kio Ave 6",
+        "Description": "Opp Castle Green",
+        "Latitude": 1.385991667,
+        "Longitude": 103.8431694
+    },
+    {
+        "BusStopCode": "55189",
+        "RoadName": "Ang Mo Kio Ave 6",
+        "Description": "Yio Chu Kang Stn",
+        "Latitude": 1.382955556,
+        "Longitude": 103.844225
+    },
+    {
+        "BusStopCode": "55209",
+        "RoadName": "Ang Mo Kio Ave 6",
+        "Description": "Opp Blk 646",
+        "Latitude": 1.37863166702811,
+        "Longitude": 103.84468444401317
+    },
+    {
+        "BusStopCode": "54059",
+        "RoadName": "Ang Mo Kio Ave 6",
+        "Description": "Bef Ang Mo Kio Lib",
+        "Latitude": 1.3762849891494,
+        "Longitude": 103.84498299185917
+    },
+    {
+        "BusStopCode": "54049",
+        "RoadName": "Ang Mo Kio Ave 6",
+        "Description": "Courts Ang Mo Kio",
+        "Latitude": 1.373029,
+        "Longitude": 103.845336
+    },
+    {
+        "BusStopCode": "54039",
+        "RoadName": "Ang Mo Kio Ave 6",
+        "Description": "Bef Al-Muttaqin Mque",
+        "Latitude": 1.37091786093364,
+        "Longitude": 103.84563650364578
+    },
+    {
+        "BusStopCode": "54241",
+        "RoadName": "Ang Mo Kio Ave 3",
+        "Description": "Blk 700B",
+        "Latitude": 1.36913518643274,
+        "Longitude": 103.84618005827467
+    },
+    {
+        "BusStopCode": "54009",
+        "RoadName": "Ang Mo Kio Ave 8",
+        "Description": "Ang Mo Kio Int",
+        "Latitude": 1.36968769913339,
+        "Longitude": 103.84856716193268
+    }
+];
