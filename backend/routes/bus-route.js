66bae5d794c5b144bda78e56732879a7d54421bd
Gabriel Teo <gtforce212@gmail.com>
2024-12-31 05:37:15 +0800

    BE: Update BusRoute API
    Merge bus stop locations in response

--- backend/routes/bus-route.js
+++ backend/routes/bus-route.js
@@ -11,23 +11,23 @@ router.get('/', async (req, res) => {
     if (req.query.Direction) {
         let busService = req.busServices.get(req.query.ServiceNo.toString()).get(req.query.Direction.toString());
         let stopsOfDirection = busRoutes.get(req.query.Direction);
-        let busStops = stopsOfDirection.map((x) =>x.BusStopCode);
+        let busStops = stopsOfDirection.map((x) => req.busStops.get(x.BusStopCode));
         res.send({
             ServiceNo : req.query.ServiceNo,
             Routes : [{
-                OriginCode : busService.OriginCode,
-                DestinationCode : busService.DestinationCode,
+                Origin : req.busStops.get(busService.OriginCode),
+                Destination : req.busStops.get(busService.DestinationCode),
                 BusStops : busStops
             }]
         });
     } else {
         let routes = [];
-        busRoutes.forEach((element, key, value) => {
+        busRoutes.forEach((element, key) => {
             let busService = req.busServices.get(req.query.ServiceNo.toString()).get(key.toString());
-            let busStops = element.map((x) =>x.BusStopCode);
+            let busStops = element.map((x) => req.busStops.get(x.BusStopCode));
             routes.push({
-                OriginCode : busService.OriginCode,
-                DestinationCode : busService.DestinationCode,
+                Origin : req.busStops.get(busService.OriginCode),
+                Destination : req.busStops.get(busService.DestinationCode),
                 BusStops : busStops
             });
         });
